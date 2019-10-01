9728aced3fc9d0868a23b8650fcada29d64e9c6a
mfrashad <m.fathyrashad@gmail.com>
2019-10-01 23:45:37 +0800

    Make drawer text styling consistent

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -46,6 +46,26 @@ const ScreenUrls = [
     url: 'https://libguides.utp.edu.my/az.php',
   },
   {
+    slug: 'remote',
+    title: 'Remote Access',
+    url: 'https://libguides.utp.edu.my/remoteaccess_app',
+  },
+  {
+    slug: 'booking',
+    title: 'Room Booking',
+    url: 'https://ibest.utp.edu.my/reserve/discussionroom',
+  },
+  {
+    slug: 'ill',
+    title: 'Interlibrary Loan (ILL)',
+    url: 'https://libguides.utp.edu.my/ircresearchsupport/ill',
+  },
+  {
+    slug: 'appointment',
+    title: 'Appointment with Librarian',
+    url: 'https://ibest.utp.edu.my/appointments/',
+  },
+  {
     slug: 'calendar',
     title: 'Calendar & Event',
     url: 'https://ibest.utp.edu.my/calendar/event/?cid=5413&t=d&d=0000-00-00&cal%5B%5D=5413',
@@ -56,34 +76,14 @@ const ScreenUrls = [
     url: 'https://libanswers.utp.edu.my/ioasisfaq/',
   },
   {
-    slug: 'remote',
-    title: 'Remote Access',
-    url: 'https://libguides.utp.edu.my/remoteaccess_app',
-  },
-  {
     slug: 'faq',
     title: 'FAQ',
     url: 'https://libanswers.utp.edu.my',
   },
   {
-    slug: 'appointment',
-    title: 'Appointment with Librarian',
-    url: 'https://ibest.utp.edu.my/appointments/',
-  },
-  {
-    slug: 'booking',
-    title: 'Room Booking',
-    url: 'https://ibest.utp.edu.my/reserve/discussionroom',
-  },
-  {
     slug: 'chat',
     title: 'Live Chat',
     url: 'https://gateway.utp.edu.my/ioasis/chat.html',
-  },
-  {
-    slug: 'ill',
-    title: 'Interlibrary Loan (ILL)',
-    url: 'https://libguides.utp.edu.my/ircresearchsupport/ill',
   },
   {
     slug: 'feedback',
@@ -125,31 +125,6 @@ const LinksStack = createStackNavigator(
   config
 );
 
-LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'} />
-  ),
-};
-
-LinksStack.path = '';
-
-const SettingsStack = createStackNavigator(
-  {
-    Settings: SettingsScreen,
-  },
-  config
-);
-
-SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'} />
-  ),
-};
-
-SettingsStack.path = '';
-
 const tabNavigator = createDrawerNavigator({
   Home: HomeStack,
   ...getScreens(ScreenUrls)
@@ -167,7 +142,12 @@ const tabNavigator = createDrawerNavigator({
   ),
   drawerOpenRoute: 'DrawerOpen',
   drawerCloseRoute: 'DrawerClose',
-  drawerToggleRoute: 'DrawerToggle'
+  drawerToggleRoute: 'DrawerToggle',
+  contentOptions: {
+    labelStyle: {
+      fontWeight: 'normal'
+    }
+  }
 });
 
 tabNavigator.path = '';
@@ -178,6 +158,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     paddingLeft: 15,
     paddingVertical: 15,
+    color: 'red'
   },
   drawerText: {
     fontWeight: 'bold',
